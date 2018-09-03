05656764b71c25a9e9b56aa2cfbd56e2774ecaf6
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-03 04:36:53 -0300

    Cria modal para edição e cadastro de novo produto

--- index.php
+++ index.php
@@ -92,10 +92,37 @@
 
         <!--Modal cadastro-->
         <div id="addnovoproduto" class="modal fade">
-            <div>
-
-            </div>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <form>
+                        <div class="modal-header">
+                            <h4 class="modal-title">Editar produto</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label>Nome</label>
+                                <input type="text" class="form-control" name="nome" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Descrição</label>
+                                <input type="text" class="form-control" name="descricao" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Preço</label>
+                                <input type="text" class="form-control" name="preco" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+                            <input type="button" class="btn btn-info" value="Salvar">
+                        </div>
+                    </form>
+                </div>
+            </div><!--/modal-dialog-->
         </div><!--/addnovoproduto-->
+
+
 
     </div><!--/div-container-->
 
