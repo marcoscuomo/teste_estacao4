04c19f097c67411802d3bfa3488c0e28ba566f55
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-03 03:30:52 -0300

    Cria botao de apagar

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
                 </div>
                 <div class="col-sm-6">
                     <a href="#" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Cadastrar novo produto</span></a>
+                    <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Apagar</span></a>
                 </div>
             </div>
         </div><!--/table-wrapper-->
