134d3d777fca7f3d7c9ddf309354ff0d6bd5a255
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-03 13:45:01 -0300

    Cria função para exibição do valor no padrão com R$

--- lista.php
+++ lista.php
@@ -7,11 +7,13 @@
     if($sql->rowCount() > 0){
 
         foreach ($sql->fetchAll() as $produto){
+            $preco = $produto['preco'];
+            $valor = 'R$' .number_format($preco, 2, ',', '.');
             echo '<tr>';
                 echo '<td>'.$produto['id'].'</td>';
                 echo '<td>'.$produto['nome'].'</td>';
                 echo '<td>'.$produto['descricao'].'</td>';
-                echo '<td>' .$produto['preco']. '</td>';
+                echo '<td>' .$valor. '</td>';
                 echo '<td>';
                     echo '<a href="editar.php?id='.$produto['id'].'" class="edit"><i class="material-icons" data-toggle="tooltip" title="Editar">&#xE254;</i></a>';
                     echo '<a href="excluir.php?id='.$produto['id'].'" class="delete"><i class="material-icons" data-toggle="tooltip" title="Excluir">&#xE872;</i></a>';
